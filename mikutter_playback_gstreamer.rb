853a6ebcf7d3f4de8a047bdb0b23a4758a907a5b
shibafu <shibafu528@gmail.com>
2014-05-25 23:04:47 +0900

    試行錯誤

--- mikutter_playback_gstreamer.rb
+++ mikutter_playback_gstreamer.rb
@@ -3,35 +3,31 @@ require 'gst'
 require_relative 'gstfix'
 
 Plugin.create :mikutter_playback_gstreamer do
-    def create_pipeline(filename)
-        audio = Gst::Bin.new("audiobin")
-        conv = Gst::ElementFactory.make("audioconvert")
-        audiopad = conv.get_static_pad("sink")
-        sink = Gst::ElementFactory.make("autoaudiosink")
-        audio << conv << sink
-        conv >> sink
-        audio.add_pad(Gst::GhostPad.new("sink", audiopad))
-
-        pipeline = Gst::Pipeline.new("pipeline")
-        src = Gst::ElementFactory.make("filesrc")
-        src.location = filename
-        decoder = Gst::ElementFactory.make("decodebin")
-        decoder.signal_connect("pad-added") do |decoder, pad|
-            audiopad = audio.get_static_pad("sink")
-            pad.link(audiopad)
-        end
-
-        pipeline << src << decoder << audio
-        src >> decoder
-
-        pipeline
-    end
-
     defplayback :gstreamer, "GStreamer" do |filename|
         Thread.new do
-            pipeline = create_pipeline(filename)
-            bus = pipeline.bus
             begin
+                audio = Gst::Bin.new("audiobin")
+                conv = Gst::ElementFactory.make("audioconvert")
+                audiopad = conv.get_static_pad("sink")
+                sink = Gst::ElementFactory.make("autoaudiosink")
+                audio << conv << sink
+                conv >> sink
+                audio.add_pad(Gst::GhostPad.new("sink", audiopad))
+
+                pipeline = Gst::Pipeline.new("pipeline")
+                src = Gst::ElementFactory.make("filesrc")
+                src.location = filename
+                decoder = Gst::ElementFactory.make("decodebin")
+                decoder.signal_connect("pad-added") do |decoder, pad|
+                    audiopad = audio.get_static_pad("sink")
+                    pad.link(audiopad)
+                end
+
+                pipeline << src << decoder << audio
+                src >> decoder
+                
+                bus = pipeline.bus
+                
                 pipeline.play
                 loop do
                     message = bus.poll(Gst::MessageType::ANY, Gst::CLOCK_TIME_NONE)
